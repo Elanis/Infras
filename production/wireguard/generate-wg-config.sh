b16249eb998f5d3cdad521a4c36e63ffe7a32ef9
Elanis <elanis@hotmail.com>
2022-07-19 18:53:12 +0200

    Production - Add new server to wireguard mesh network

--- production/wireguard/generate-wg-config.sh
+++ production/wireguard/generate-wg-config.sh
@@ -3,9 +3,10 @@ set -euo pipefail
 
 wg-meshconf init
 
-wg-meshconf addpeer --address 192.168.1.2 --endpoint helium.dysnomia helium.dysnomia.studio
-wg-meshconf addpeer --address 192.168.1.3 --endpoint lithium.dysnomia lithium.dysnomia.studio
-wg-meshconf addpeer --address 192.168.1.4 --endpoint beryllium.dysnomia beryllium.dysnomia.studio
+wg-meshconf addpeer --address 192.168.1.2 --endpoint helium.dysnomia.studio helium.dysnomia.studio
+wg-meshconf addpeer --address 192.168.1.3 --endpoint lithium.dysnomia.studio lithium.dysnomia.studio
+wg-meshconf addpeer --address 192.168.1.4 --endpoint beryllium.dysnomia.studio beryllium.dysnomia.studio
+wg-meshconf addpeer --address 192.168.1.5 --endpoint bore.dysnomia.studio bore.dysnomia.studio
 
 wg-meshconf genconfig
 
