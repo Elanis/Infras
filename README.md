b360d910925d76b029ab83457cb2214bb6d12f66
Elanis <elanis@hotmail.com>
2024-12-17 18:37:02 +0100

    Production/Ansible - Upgrade to the latest k3s-io/k3s-ansible version, enable IPV6 support and dual-stack CNI, switch to the new servers list

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 This repository hosts my home lab and production (Dysnomia) infrastructure files.  
 
 Note: I am more of a developer than an ops, but I try to learn. Don't hesitate to open an issue/PR if you think I made some mistake :)
+
+The K3S-related parts of this repos are heavily copied from [https://github.com/k3s-io/k3s-ansible](https://github.com/k3s-io/k3s-ansible) licensed under Apache License 2.0.
+The other parts of this repo are licensed under MIT license as follows.
--- production/wireguard/generate-wg-config.sh
+++ production/wireguard/generate-wg-config.sh
@@ -3,10 +3,10 @@ set -euo pipefail
 
 wg-meshconf init
 
-wg-meshconf addpeer --address 192.168.1.2 --endpoint helium.dysnomia.studio helium.dysnomia.studio
-wg-meshconf addpeer --address 192.168.1.3 --endpoint lithium.dysnomia.studio lithium.dysnomia.studio
-wg-meshconf addpeer --address 192.168.1.4 --endpoint beryllium.dysnomia.studio beryllium.dysnomia.studio
-wg-meshconf addpeer --address 192.168.1.5 --endpoint bore.dysnomia.studio bore.dysnomia.studio
+wg-meshconf addpeer --address 192.168.1.1 --endpoint local.dysnomia.studio local.dysnomia.studio
+wg-meshconf addpeer --address 192.168.1.6 --endpoint carbon.dysnomia.studio carbon.dysnomia.studio
+wg-meshconf addpeer --address 192.168.1.7 --endpoint nitrogen.dysnomia.studio nitrogen.dysnomia.studio
+wg-meshconf addpeer --address 192.168.1.8 --endpoint oxygen.dysnomia.studio oxygen.dysnomia.studio
 
 wg-meshconf genconfig
 
